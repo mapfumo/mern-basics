c05483ae000d498810c2f7ee2b1df4cffbc5a133
Tony <mapfumo@gmail.com>
2024-09-21 06:53:04 +1000

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,3 @@ app.listen(5000, () => {
   connectDB();
   console.log("Server is running at http://localhost:5000");
 });
-
-// 9UK2wlLzXu4hA3ac
-
-// mongodb+srv://mapfumo:9UK2wlLzXu4hA3ac@cluster0.0yei5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
---
+++ models/product.models.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // Adds createdAt and updatedAt fields
+  }
+);
+
+export const Product = mongoose.model("Product", productSchema);
